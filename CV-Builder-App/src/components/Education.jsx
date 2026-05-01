60c200da52cc1f7bcc6cd62d473349ff78c6c66f
Aayush Trivedi <aayush.t200@gmail.com>
2026-05-01 15:43:43 +0530

    feat: enhance Education component with dynamic data handling and styling

--- CV-Builder-App/src/components/Education.jsx
+++ CV-Builder-App/src/components/Education.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import '../styles/Education.css';
 export default function Education({ data, onSave }) {
     const [viewMode, setViewMode] = useState('preview');
     function updateView() {
@@ -48,10 +48,10 @@ function UniversityInfo({ data }) {
     );
 }
 function Edit({ data, onSubmit, onSave }) {
-    const [newData, setNewData] = useState(data);
+    const [newData, updateData] = useState(data);
     function handleAdd(e) {
         e.preventDefault();
-        setNewData((prev) => [
+        updateData((prev) => [
             ...prev,
             {
                 id: prev.length ? prev.at(-1).id + 1 : 1,
@@ -62,7 +62,12 @@ function Edit({ data, onSubmit, onSave }) {
             },
         ]);
     }
-
+    function updateDataValues(updatedData) {
+        updateData(newData.map((item) => (item.id === updatedData.id ? updatedData : item)));
+    }
+    function handleRemove(id) {
+        updateData(newData.filter((university) => university.id !== id));
+    }
     function handleSubmit(e) {
         e.preventDefault();
         onSave('EducationData', newData);
@@ -73,8 +78,14 @@ function Edit({ data, onSubmit, onSave }) {
         <form onSubmit={handleSubmit}>
             <div className="formControl">
                 {newData.map((university) => (
-                    <UniversityInfoEdit data={university} key={university.id}></UniversityInfoEdit>
+                    <UniversityInfoEdit
+                        data={university}
+                        key={university.id}
+                        onChange={updateDataValues}
+                        onRemove={handleRemove}
+                    ></UniversityInfoEdit>
                 ))}
+
                 <button type="button" className="btn" onClick={handleAdd}>
                     Add New
                 </button>
@@ -85,16 +96,80 @@ function Edit({ data, onSubmit, onSave }) {
         </form>
     );
 }
-function UniversityInfoEdit({ data }) {
-    console.log(data);
-    return <>{data.universityName}</>;
+function UniversityInfoEdit({ data, onChange, onRemove }) {
+    const [newData, setNewData] = useState(data);
+
+    function handleEdit(e) {
+        const updated = {
+            ...newData,
+            [e.target.id]: e.target.value,
+        };
+        setNewData(updated);
+        onChange(updated);
+    }
+    return (
+        <div className="UniversityInfoUpdate">
+            <Input
+                id="universityName"
+                type="text"
+                value={newData.universityName}
+                onChange={handleEdit}
+                label="University Name"
+                pattern="^[A-Za-z0-9\s.,&()-]{2,}$"
+                title="Enter a valid university name"
+            ></Input>
+            <Input
+                id="graduationDate"
+                type="text"
+                value={newData.graduationDate}
+                onChange={handleEdit}
+                label="Graduation Date"
+                pattern="^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{4}$"
+                title="Format: May 2023"
+            ></Input>
+            <Input
+                id="degreeName"
+                type="text"
+                value={newData.degreeName}
+                onChange={handleEdit}
+                label="Degree Name"
+                pattern="^[A-Za-z0-9\s.,()-]{2,}$"
+                title="Enter a valid degree name"
+            ></Input>
+            <label htmlFor="notableCourses">
+                Notable Courses :
+                <textarea
+                    id="notableCourses"
+                    type="text"
+                    value={newData.notableCourses}
+                    onChange={handleEdit}
+                    cols={50}
+                    rows={2}
+                    pattern="^.{5,}$"
+                    title="Enter at least 5 characters"
+                ></textarea>
+            </label>
+            <button type="Button" onClick={() => onRemove(newData.id)} className="btn">
+                ❌
+            </button>
+        </div>
+    );
 }
 
 function Input({ id, type, value, onChange, label, pattern, title }) {
     return (
         <>
             <label htmlFor={id}>{label} :</label>
-            <input type={type} value={value} onChange={onChange} id={id} name={id} pattern={pattern} title={title} />
+            <input
+                type={type}
+                value={value}
+                onChange={onChange}
+                id={id}
+                name={id}
+                pattern={pattern}
+                title={title}
+                required
+            />
         </>
     );
 }
